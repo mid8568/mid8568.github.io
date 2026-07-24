313c45d9c4e1768cddee3a9127940fe660cc3803
mid8568 <56178383@qq.com>
2026-07-24 11:31:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+//====================
 // 打开入学时间筛选
+//====================
 
 async function openYearFilter(){
 
 
 let {data,error}=await db
-
 .from("students")
-
 .select("year");
-
 
 
 if(error){
@@ -20,16 +19,14 @@ return;
 }
 
 
-// 去重
+// 获取数据库已有年份
 
 let years=[
 
 ...new Set(
 
 data
-
 .map(s=>s.year)
-
 .filter(Boolean)
 
 )
@@ -56,9 +53,7 @@ ${y}
 
 `;
 
-
 });
-
 
 
 document.getElementById("yearList").innerHTML=html;
@@ -67,6 +62,133 @@ document.getElementById("yearList").innerHTML=html;
 
 document.getElementById("yearModal")
 .style.display="block";
+
+
+}
+
+
+
+
+//====================
+// 关闭入学时间筛选
+//====================
+
+
+function closeYearFilter(){
+
+
+document.getElementById("yearModal")
+.style.display="none";
+
+
+}
+
+
+
+
+//====================
+// 按年份筛选
+//====================
+
+
+async function filterByYear(year){
+
+
+
+let {data,error}=await db
+
+.from("students")
+
+.select("*")
+
+.ilike(
+"year",
+`%${year}%`
+)
+
+.order("id");
+
+
+
+if(error){
+
+alert(error.message);
+
+return;
+
+}
+
+
+
+let html="";
+
+
+data.forEach(s=>{
+
+
+html+=`
+
+<tr>
+
+<td>${s.id||""}</td>
+
+
+<td>
+
+<a onclick="showStudent(${s.id})">
+
+${s.name||""}
+
+</a>
+
+</td>
+
+
+<td>${s.school||""}</td>
+
+
+<td>${s.idcard||""}</td>
+
+
+<td>${s.phone||""}</td>
+
+
+<td>${s.gender||""}</td>
+
+
+<td>${s.major||""}</td>
+
+
+<td>${s.level||""}</td>
+
+
+<td>${s.year||""}</td>
+
+
+<td>
+
+<button onclick="editStudent(${s.id})">
+
+编辑
+
+</button>
+
+</td>
+
+
+</tr>
+
+`;
+
+});
+
+
+
+document.getElementById("list").innerHTML=html;
+
+
+
+closeYearFilter();
 
 
 }
